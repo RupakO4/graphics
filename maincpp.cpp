c5bb0546705128cfd685b10f034143145e48962d
RupakO4 <47048146+RupakO4@users.noreply.github.com>
2021-08-06 10:33:45 +0545

    nc

--- maincpp.cpp
+++ maincpp.cpp
@@ -5,7 +5,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include "glm/gtx/string_cast.hpp" //pangeni
+#include "glm/gtx/string_cast.hpp"
 
 #include "Shader.h"
 #include "Camera.h"
